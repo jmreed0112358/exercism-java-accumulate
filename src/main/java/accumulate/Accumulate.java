6658b131acb802e7aaa0be3046064a6f6f63bf26
Jeremy M. Reed <reeje76@gmail.com>
2016-06-29 18:06:53 -0600

    Project setup.
    Import into Eclipse.
    Convert to Maven project.
    Add JUnit 4.10 dependency.
    Move sources to proper packages.
    Add NotImplementedException RuntimeException.
    Stub out Accumulate.

---
+++ src/main/java/accumulate/Accumulate.java
@@ -0,0 +1,13 @@
+package accumulate;
+
+import java.util.List;
+import java.util.function.Function;
+
+import exceptions.NotImplementedException;
+
+public class Accumulate
+{
+	public static <T, R> List<T> accumulate(List<T> input, Function<T, R> rule) {
+		throw new NotImplementedException();
+	}
+}
--- src/test/java/accumulate/AccumulateTest.java
+++ src/test/java/accumulate/AccumulateTest.java
@@ -1,3 +1,4 @@
+package accumulate;
 import org.junit.Test;
 
 import java.util.Arrays;
