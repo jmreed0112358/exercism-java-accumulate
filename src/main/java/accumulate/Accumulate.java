5bd7de8458630f4b26763d4b04df6ebd334d4ac5
Jeremy M. Reed <reeje76@gmail.com>
2016-06-29 18:17:21 -0600

    Implement Accumulate.

--- src/main/java/accumulate/Accumulate.java
+++ src/main/java/accumulate/Accumulate.java
@@ -1,13 +1,22 @@
 package accumulate;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Function;
 
-import exceptions.NotImplementedException;
-
 public class Accumulate
 {
-	public static <T, R> List<T> accumulate(List<T> input, Function<T, R> rule) {
-		throw new NotImplementedException();
+	public static <T, R> List<R> accumulate(List<T> input, Function<T, R> rule) {
+		if ( input == null || rule == null ) {
+			throw new NullPointerException();
+		}
+		
+		List<R> result = new LinkedList<R>();
+
+		for ( int i = 0 ; i < input.size( ) ; i++ ) {
+			result.add( rule.apply( input.get( i ) ) );
+		}
+		
+		return result;
 	}
 }
